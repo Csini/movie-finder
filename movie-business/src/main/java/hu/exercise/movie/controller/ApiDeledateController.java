f58b0e8cb2486e0d7d00438141d55438e96be514
csini <csini@gmx.com>
2024-10-18 18:13:08 +0200

    ISSUE-2: basic version from openapi.yaml is ready

--- movie-business/src/main/java/hu/exercise/movie/controller/ApiDeledateController.java
+++ movie-business/src/main/java/hu/exercise/movie/controller/ApiDeledateController.java
@@ -2,14 +2,16 @@ package hu.exercise.movie.controller;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import hu.exercise.movie.openapi.api.DefaultApiDelegate;
+import hu.exercise.movie.openapi.api.MovieNameApiDelegate;
+import hu.exercise.movie.openapi.model.MovieResponse;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
-public class ApiDeledateController implements DefaultApiDelegate {
+public class ApiDeledateController implements MovieNameApiDelegate {
 
 	private ModelMapper beanMapper;
 
@@ -22,4 +24,9 @@ public class ApiDeledateController implements DefaultApiDelegate {
 		this.beanMapper = beanMapper;
 	}
 
+	@Override
+	public ResponseEntity<MovieResponse> searchByMovieName(String apiName, String movieName) {
+		// TODO call MovieService
+		return MovieNameApiDelegate.super.searchByMovieName(apiName, movieName);
+	}
 }
